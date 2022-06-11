05a142072d232957a68a2eb2c65a607dc5d37c79
rahulshuvo <rahulshuvo5@gmail.com>
2022-06-11 17:48:17 +0600

    initial commit

--- app/build/generated/source/buildConfig/debug/com/service/sohan/nfcreadwritetest/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/service/sohan/nfcreadwritetest/BuildConfig.java
@@ -7,7 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.service.sohan.nfcreadwritetest";
   public static final String BUILD_TYPE = "debug";
-  public static final String FLAVOR = "";
   public static final int VERSION_CODE = 11;
   public static final String VERSION_NAME = "3.0.7";
 }
